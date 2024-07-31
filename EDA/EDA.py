3c893b4de86e0ce6c4a5621bbc83c7f006a73fb8
JhormanGonzalez <jgonzalez202303@gmail.com>
2024-07-31 15:30:05 -0500

    Functions to get initial summary of columns and outlliers from csv files based on json parameters added

--- EDA/EDA.py
+++ EDA/EDA.py
@@ -5,74 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from ydata_profiling import ProfileReport
 import os
-
-
-def get_math_stats(df, column):
-    # Convert the column to numeric, coercing errors to NaN
-    df[column] = pd.to_numeric(df[column], errors='coerce')
-    # Drop NaN values
-    cleaned_col = df[column].dropna()
-    max_val = cleaned_col.max()
-    min_val = cleaned_col.min()
-    median_val = cleaned_col.median() if np.issubdtype(cleaned_col.dtype, np.number) else None
-    return max_val, min_val, median_val
-
-
-def str_to_num(val):
-    try:
-        float(val)
-        return float(val)
-    except:
-        return str(val)
-
-
-def column_summary(df):
-    summary = pd.DataFrame(columns=['num_rows', 'num_values', 'num_unique_values', 'num_empties', 'max', 'min', 'median',
-                                    'num_numeric', 'num_strings', 'num_undefined',
-                                    'percentage_numeric', 'percentage_strings', 'percentage_undefined'])
-    
-    num_rows = len(df)
-    for col in df.columns:
-        num_values = df[col].count()
-        num_unique_values = df[col].nunique()
-        num_empties = df[col].isnull().sum()
-        
-        num_numeric = df[col].apply(lambda x: isinstance(str_to_num(x), (int, float))).sum()
-        num_strings = df[col].apply(lambda x: isinstance(str_to_num(x), str)).sum()
-        #num_dates = df[col].apply(lambda x: isinstance(x, pd.Timestamp)).sum()
-        num_undefined = num_rows - (num_numeric + num_strings)
-        
-        percentage_numeric = num_numeric / num_rows if num_rows > 0 else 0
-        percentage_strings = num_strings / num_rows if num_rows > 0 else 0
-        percentage_undefined = num_undefined / num_rows if num_rows > 0 else 0
-
-        max_val, min_val, median_val = get_math_stats(df, col)
-        
-        summary.loc[col] = [num_rows, num_values, num_unique_values, num_empties, max_val, min_val, median_val,
-                            num_numeric, num_strings, num_undefined,
-                            percentage_numeric, percentage_strings, percentage_undefined]
-    
-    return summary
-
-def identify_outliers(df_summary, threshold=0.8):
-    outliers = {}
-    for col in df_summary.index:
-        percentages = df_summary.loc[col, ['percentage_numeric', 'percentage_strings', 'percentage_dates', 'percentage_undefined']]
-        max_percentage_type = percentages.idxmax()
-        if percentages[max_percentage_type] < threshold:
-            outliers[col] = df_summary.loc[col].to_dict()
-    
-    return outliers
-
-def analyze_dataframe(df):
-    summary = column_summary(df)
-    #outliers = identify_outliers(df, summary)
-    #summary['outliers_details'] = summary.index.map(outliers.get)
-    print(summary)
-    summary.to_csv("assets/summary.csv")
-    #for column, outlier_info in outliers.items():
-    #    print(f"{column}:")
-    #    print(outlier_info)
+import json
 
 
 def check_dict_fields(data_dict: dict, params_required: list):
@@ -96,7 +29,83 @@ def load_data(source_metadata: dict) -> pd.DataFrame:
         return pd.read_csv(file_path, sep=field_terminator)
 
 
-def run_EDA(df: pd.DataFrame):
+def get_math_stats(df, column):
+    # Convert the column to numeric, coercing errors to NaN
+    df[column] = pd.to_numeric(df[column], errors='coerce')
+    # Drop NaN values
+    cleaned_col = df[column].dropna()
+    max_val = cleaned_col.max()
+    min_val = cleaned_col.min()
+    median_val = cleaned_col.median() if np.issubdtype(cleaned_col.dtype, np.number) else None
+    return max_val, min_val, median_val
+
+
+def identify_outliers(column, num_values, str_values):
+    target_values = str_values
+    # String values are outliers
+    if len(num_values) < len(str_values):
+        # Get unique string values
+        target_values = num_values
+    
+    unique_outliers = list(set(target_values))
+    outliers = [{'column': column, 'value': outlier, 'count': target_values.count(outlier)} for outlier in unique_outliers]
+
+    return outliers
+
+
+def str_to_num(val):
+    try:
+        float(val)
+        return float(val)
+    except:
+        return str(val)
+
+
+def column_summary(df):
+    rows = []
+    num_rows = len(df)
+    outliers = []
+
+    for col in df.columns:
+        num_values = df[col].count()
+        num_unique_values = df[col].nunique()
+        num_empties = df[col].isnull().sum()
+        
+        num_values = df[col].apply(lambda x: x if isinstance(str_to_num(x), (int, float)) else np.nan).dropna().values.tolist()
+        num_numeric = len(num_values)
+        str_values = df[col].apply(lambda x: x if isinstance(str_to_num(x), str) else np.nan).dropna().values.tolist()
+        num_strings = len(str_values)
+        # Get outliers
+        outliers.extend(identify_outliers(col, num_values, str_values))
+        #num_dates = df[col].apply(lambda x: isinstance(x, pd.Timestamp)).sum()
+        num_undefined = num_rows - (num_numeric + num_strings)
+        
+        percentage_numeric = num_numeric / num_rows if num_rows > 0 else 0
+        percentage_strings = num_strings / num_rows if num_rows > 0 else 0
+        percentage_undefined = num_undefined / num_rows if num_rows > 0 else 0
+
+        max_val, min_val, median_val = get_math_stats(df, col)
+        
+        rows.append(
+            [
+                col, num_rows, num_numeric, num_unique_values, num_empties,
+                max_val, min_val, median_val,
+                num_numeric, num_strings, num_undefined,
+                percentage_numeric, percentage_strings, percentage_undefined
+            ]
+        )
+    
+    columns = [
+        'columns', 'num_rows', 'num_values', 'num_unique_values', 'num_empties',
+        'max', 'min', 'median','num_numeric', 'num_strings', 'num_undefined',
+        'percentage_numeric', 'percentage_strings', 'percentage_undefined'
+    ]
+    summary = pd.DataFrame(rows, columns=columns)
+
+    return summary, pd.DataFrame(outliers)
+
+
+def run_EDA(df: pd.DataFrame, output_path: str):
     # BASIC INFORMATION #
     # Display the first few rows of the dataframe
     print(df.head())
@@ -113,46 +122,49 @@ def run_EDA(df: pd.DataFrame):
     # Numerical Features #
     # Histograms for numerical columns
     df.hist(bins=30, figsize=(15, 10))
-    plt.show()
-    # Boxplots for numerical columns
-    df.plot(kind='box', subplots=True, layout=(4, 4), figsize=(15, 10), sharex=False, sharey=False)
-    plt.show()
-
-    # Categorical Features #
-    # Count plots for categorical columns
-    #for column in df.select_dtypes(include=['object']).columns:
-    #    sns.countplot(y=column, data=df)
-    #    plt.show()
-
-    # CORRELATION ANALYSIS #
-    # Correlation matrix
-    #corr_matrix = df.corr()
-    #sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
-    #plt.show()
+    plt.savefig(output_path + "/histogram.png")
 
     # AUTOMATED REPORTS #
     # Generate a pandas profiling report
     profile = ProfileReport(df)
-    profile.to_file("assets/data_report.html")
+    profile.to_file(output_path + "/data_report.html")
+
+
+def analyze_dataframe(df: pd.DataFrame, output_path: str):
+    summary, outliers = column_summary(df)
+    summary.to_csv(output_path + "/summary.csv")
+    outliers.to_csv(output_path + "/outliers.csv")
 
 
 def main():
-    metadata_dict = {
-        "file_type": "csv",
-        "file_path": "data/train_A.csv",
-        "field_terminator": "|"
-    }
-    # Create assets folder if doesn't exist
-    if not os.path.exists('assets'):
-        os.makedirs('assets')
-    
-    df_brute = load_data(metadata_dict)
-    # Run EDA
-    print("Running EDA...")
-    run_EDA(df_brute)
-    # Run Column Explorer
-    print("Running Column Explorer...")
-    analyze_dataframe(df_brute)
+    base_ans_folder = 'assets/initial_exploration'
+
+    load_parameters_list = json.loads(open('data/parameters.json').read())["filesystem"]["load"]
+
+    for load_parameters in load_parameters_list:
+        metadata_dict = load_parameters
+        file_paths = load_parameters['file_paths']
+        for file_path in file_paths:
+            print(f"\nProcessing file: '{file_path}'\n")
+            # Use load parameters for every file path
+            metadata_dict['file_path'] = file_path
+            print(f"Parameters to load:")
+            for key, val in metadata_dict.items():
+                print(f"\t{key}: {val}")
+            # Create subfolder witoho file name of current file path
+            file_name = os.path.basename(file_path).split(".")[-2]
+            output_path = base_ans_folder + "/" + file_name
+            if not os.path.exists(output_path):
+                os.makedirs(output_path)
+
+            # Load tabel to analize
+            df_brute = load_data(metadata_dict)
+            # Run EDA
+            print("Running EDA...")
+            run_EDA(df_brute, output_path)
+            # Run Column Explorer
+            print("Running Column Explorer...")
+            analyze_dataframe(df_brute, output_path)
 
 
 if __name__ == '__main__':
